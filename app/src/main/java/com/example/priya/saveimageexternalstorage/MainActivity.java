66e84854c6e0762140d517d881bfc464d2796348
talktoyaminipriya <talktoyaminipriya@gmail.com>
2017-12-01 00:27:25 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/priya/saveimageexternalstorage/MainActivity.java
+++ app/src/main/java/com/example/priya/saveimageexternalstorage/MainActivity.java
@@ -11,7 +11,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.jar.Manifest;
+import android.Manifest;
 
 public class MainActivity extends AppCompatActivity {
 
